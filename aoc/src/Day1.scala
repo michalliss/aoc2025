d79eaf8eaec1f8614831bee8cd29c833fa3127c1
michalliss <michal.j.liss@gmail.com>
2025-12-01 21:46:58 +0100

    day 1

--- aoc/src/Day1.scala
+++ aoc/src/Day1.scala
@@ -1,5 +1,88 @@
 package aoc
 
+import fastparse.*
+import fastparse.Parsed.{Failure, Success}
+import zio.*
+import zio.stream.*
+
+import NoWhitespace.*
+
 object Day1 {
-    
+
+  enum Direction:
+    case L
+    case R
+
+  case class Rotation(direction: Direction, steps: Int)
+
+  case class RotationResult(newDial: Dial, crossedZeroCount: Int)
+
+  case class Dial private (position: Int):
+    def rotate(rotation: Rotation): Dial =
+      rotateDetailed(rotation).newDial
+
+    def rotateDetailed(rotation: Rotation): RotationResult =
+      val newRawPosition = rotation.direction match
+        case Direction.L => (position - rotation.steps)
+        case Direction.R => (position + rotation.steps)
+
+      val signChanged  = position != 0 && newRawPosition != 0 && position.sign != newRawPosition.sign
+      val landedOnZero = newRawPosition == 0
+
+      val crossingFromWraps         = Math.abs(newRawPosition / 100)
+      val crossingFromSignChange    = if signChanged then 1 else 0
+      val crossingFromLandingOnZero = if landedOnZero then 1 else 0
+      val allCrossings              = crossingFromWraps + crossingFromSignChange + crossingFromLandingOnZero
+
+      val newDial =
+        Dial(newRawPosition % 100 match
+          case x if x < 0 => x + 100
+          case x          => x)
+
+      RotationResult(newDial, allCrossings)
+
+  object Dial:
+    def initial = Dial(50)
+
+  object parsing {
+    def number[$: P]    = P(CharIn("0-9").rep(1).!.map(_.toInt))
+    def direction[$: P] = P(CharIn("LR").!).map({
+      case "L" => Direction.L
+      case "R" => Direction.R
+    })
+    def rotation[$: P]  = P(direction ~ number).map { case (dir, num) => Rotation(dir, num) }
+
+    def parseRotation(input: String) = parse(input, c => rotation(using c)) match
+      case Success(value, index) => Some(value)
+      case _: Failure            => None
+  }
+
+  def solutionPart1(input: ZStream[Any, Throwable, Byte]) =
+    for {
+      res <- input
+               .via(ZPipeline.utfDecode)
+               .via(ZPipeline.splitLines)
+               .map(parsing.parseRotation)
+               .some
+               .runFold((zeroCount = 0, dial = Dial.initial)) { case ((zeroCount, dial), rotation) =>
+                 val newDial      = dial.rotate(rotation)
+                 val newZeroCount = if (newDial.position == 0) zeroCount + 1 else zeroCount
+                 (newZeroCount, newDial)
+               }
+    } yield res.zeroCount
+
+  def solutionPart2(input: ZStream[Any, Throwable, Byte]) =
+    for {
+      res <- input
+               .via(ZPipeline.utfDecode)
+               .via(ZPipeline.splitLines)
+               .map(parsing.parseRotation)
+               .some
+               .runFold((zeroCount = 0, dial = Dial.initial)) { case ((zeroCount, dial), rotation) =>
+                 val rotationResult = dial.rotateDetailed(rotation)
+                 val newZeroCount   = zeroCount + rotationResult.crossedZeroCount
+                 (newZeroCount, rotationResult.newDial)
+               }
+    } yield res.zeroCount
+
 }
--- aoc/test/src/Day1Spec.scala
+++ aoc/test/src/Day1Spec.scala
@@ -1,10 +1,36 @@
 import zio.*
 import zio.test.*
+import zio.stream.*
 
 object Day1Spec extends ZIOSpecDefault {
+
+  val example = (ZStream.fromResource("day1/example.txt")).orDie
+  val data    = (ZStream.fromResource("day1/data.txt")).orDie
+
   def spec = suite("Day1Spec")(
-    test("example test") {
-      assertTrue(1 + 1 == 2)
-    }
+    suite("part1")(
+      test("example") {
+        for {
+          result <- aoc.Day1.solutionPart1(example)
+        } yield assertTrue(result == 3)
+      },
+      test("data") {
+        for {
+          result <- aoc.Day1.solutionPart1(data)
+        } yield assertTrue(result == 1147)
+      }
+    ),
+    suite("part2")(
+      test("example") {
+        for {
+          result <- aoc.Day1.solutionPart2(example)
+        } yield assertTrue(result == 6)
+      },
+      test("data") {
+        for {
+          result <- aoc.Day1.solutionPart2(data)
+        } yield assertTrue(result == 6789)
+      }
+    )
   )
 }
